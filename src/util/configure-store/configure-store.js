630e22383eb089e92c7194ffcb56c261b4081654
Joey Schroeder <schrjos@gmail.com>
2019-08-20 16:35:36 -0500

    fix: Fix hot module store replacement

--- src/util/configure-store/configure-store.js
+++ src/util/configure-store/configure-store.js
@@ -1,8 +1,8 @@
 import { applyMiddleware, createStore } from 'redux';
 
-import { createRootReducer } from 'store/create-root-reducer';
 import { routerMiddleware } from 'connected-react-router';
 import thunk from 'redux-thunk';
+import { createRootReducer } from 'store/create-root-reducer';
 
 export const configureStore = history => {
   const store = createStore(createRootReducer(history), applyMiddleware(routerMiddleware(history), thunk));
@@ -11,7 +11,7 @@ export const configureStore = history => {
   // This is ignored during testing because there is
   // no way to mock npm's "module" during testing.
   if (module.hot) {
-    module.hot.accept('../store/create-root-reducer', () => {
+    module.hot.accept('../../store/create-root-reducer', () => {
       // eslint-disable-next-line global-require
       const nextReducers = require('../../store/create-root-reducer');
       store.replaceReducer(nextReducers);
