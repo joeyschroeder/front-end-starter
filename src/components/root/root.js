0945b4ca5ec649e10e5668c94d1744fb5168fb2e
Joey Schroeder <schrjos@gmail.com>
2020-06-12 19:11:02 -0500

    fix: Add basename to router in production mode
    Previously, when the app was deployed to GitHub Pages the initial route
    would not load because GitHub Pages prefixes the deployment with the
    repository name.  This has been corrected.

--- src/components/root/root.js
+++ src/components/root/root.js
@@ -6,13 +6,19 @@ import React from 'react';
 import { ReduxViewConnected } from 'components/redux-view/redux-view.connected';
 import { Route, BrowserRouter } from 'react-router-dom';
 import { Splash } from 'components/splash/splash';
+import { name } from '../../../package.json';
+
+// The application is deployed to GitHub Pages. GitHub Pages prefixes the
+// application root with the package name: 'front-end-starter' so we use is as
+// the basename for the router when in 'production' mode'
+const basename = process.env.BABEL_ENV === 'production' ? name : '';
 
 export const Root = (props) => {
   const { store } = props;
 
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={basename}>
         <Main>
           <Route exact path={ROUTES.SPLASH} component={Splash} />
           <Route exact path={ROUTES.REDUX} component={ReduxViewConnected} />
