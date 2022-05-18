9aa0984a09ca6b82d9b805147f27156729760b4c
Joey Schroeder <schrjos@gmail.com>
2022-05-18 11:34:41 -0500

    feat: Add color prop to BlockButton

--- src/components/block-button/block-button.js
+++ src/components/block-button/block-button.js
@@ -1,12 +1,17 @@
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
 import styles from './block-button.style.scss';
 
 export function BlockButton(props) {
-  const { children, onClick } = props;
+  const { children, onClick, color } = props;
+
+  const className = classNames(styles.root, {
+    [styles[color]]: color,
+  });
 
   return (
-    <button className={styles.root} type="button" onClick={onClick}>
+    <button className={className} type="button" onClick={onClick}>
       {children}
     </button>
   );
@@ -17,10 +22,12 @@ BlockButton.propTypes = {
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
   ]),
+  color: PropTypes.string,
   onClick: PropTypes.func,
 };
 
 BlockButton.defaultProps = {
   children: null,
+  color: undefined,
   onClick: null,
 };
